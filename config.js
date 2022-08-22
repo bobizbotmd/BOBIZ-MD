79cc85aed238480760dfcb58efc1ba1b4b857c20
silana-aqua-boy <111536657+silana-aqua-boy@users.noreply.github.com>
2022-08-22 21:35:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     URL_1LINK: process.env.URL_1LINK === undefined ? 'http://instagram.com/noureddine_ouafy' : process.env.URL_1LINK,
     URL_2NAME: process.env.URL_2NAME === undefined ? '✨Star on Github✨' : process.env.URL_2NAME,
     URL_2LINK: process.env.URL_2LINK === undefined ? 'https://github.com/sanuwaofficial' : process.env.URL_2LINK,
-    FOOTER: process.env.FOOTER === undefined ? 'BOBIZBOT-MD  instagram.com/noureddine_ouafy تابعني على الانستغرام ' : process.env.FOOTER,
-    CAPTION: process.env.CAPTION === undefined ? '🐶𝙱𝙾𝙱𝙸𝚉 𝙱𝙾𝚃 𝙼𝙳🐕' : process.env.CAPTION,
+    FOOTER: process.env.FOOTER === undefined ? 'علي،الواقدي-MD  instagram.com/noureddine_ouafy تابعني على الانستغرام ' : process.env.FOOTER,
+    CAPTION: process.env.CAPTION === undefined ? '🐶علي،الواقدي🐕' : process.env.CAPTION,
     ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
     ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://telegra.ph/file/201b5c02be1e336a29c60.jpg' : process.env.ALIVE_LOGO,
     SONG_DOWN: process.env.SONG_DOWN === undefined ? '*📥Downloading your song...*' : process.env.SONG_DOWN,
@@ -27,4 +27,55 @@ module.exports = {
     FILE_DOWN: process.env.FILE_DOWN === undefined ? '*📥Downloading your file...*' : process.env.FILE_DOWN,
     FILE_UP: process.env.FILE_UP === undefined ? '*📤Uploading your file...*' : process.env.FILE_UP,
     STIC_WM:process.env.STIC_WM === undefined ? 'BOBIZ-MD' : process.env.STIC_WM,
+};
+const { Sequelize } = require('sequelize');
+
+const fs = require('fs');
+
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+
+    return text === fault ? true : false;
+
+}
+
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './tmp' : process.env.DATABASE_URL;
+
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+
+module.exports = {
+
+    SESSION: process.env.BOBIZ_SESSION === undefined ? '' : process.env.BOBIZ_SESSION,
+
+    URL_1NAME: process.env.URL_1NAME === undefined ? '🎃Follow me on Instagram🎃' : process.env.URL_1NAME,
+
+    URL_1LINK: process.env.URL_1LINK === undefined ? 'http://instagram.com/noureddine_ouafy' : process.env.URL_1LINK,
+
+    URL_2NAME: process.env.URL_2NAME === undefined ? '✨Star on Github✨' : process.env.URL_2NAME,
+
+    URL_2LINK: process.env.URL_2LINK === undefined ? 'https://github.com/sanuwaofficial' : process.env.URL_2LINK,
+
+    FOOTER: process.env.FOOTER === undefined ? 'BOBIZBOT-MD  https://youtu.be/WZ-ln1n4-WQ  على الانستغرام ' : process.env.FOOTER,
+
+    CAPTION: process.env.CAPTION === undefined ? '🐶علي،الواقدي 🐕' : process.env.CAPTION,
+
+    ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
+
+    ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://badbotz.jpg/file/201b5c02be1e336a29c60.jpg' : process.env.ALIVE_LOGO,
+
+    SONG_DOWN: process.env.SONG_DOWN === undefined ? '*📥Downloading your song...*' : process.env.SONG_DOWN,
+
+    SONG_UP: process.env.SONG_UP === undefined ? '*📤Uploading your song...*' : process.env.SONG_UP,
+
+    VIDEO_DOWN: process.env.VIDEO_DOWN === undefined ? '*📥Downloading your video...*' : process.env.VIDEO_DOWN,
+
+    VIDEO_UP: process.env.VIDEO_UP === undefined ? '*📤Uploading your video...*' : process.env.VIDEO_UP,
+
+    FILE_DOWN: process.env.FILE_DOWN === undefined ? '*📥Downloading your file...*' : process.env.FILE_DOWN,
+
+    FILE_UP: process.env.FILE_UP === undefined ? '*📤Uploading your file...*' : process.env.FILE_UP,
+
+    STIC_WM:process.env.STIC_WM === undefined ? 'BOBIZ-MD' : process.env.STIC_WM,
+
 };
